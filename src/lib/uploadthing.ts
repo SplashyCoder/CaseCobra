1eabc2a6260a0ab922e446f444abe5b9f949c9dc
SplashyCoder <pachecomorahectordavid@gmail.com>
2024-09-28 16:31:55 -0500

    Uploadathing problem not fixed yet

--- src/lib/uploadthing.ts
+++ src/lib/uploadthing.ts
@@ -1,3 +1,9 @@
+// import { OurFileRouter } from "@/app/api/uploadthing/core";
+// import { generateReactHelpers } from "@uploadthing/react";
+
+// export const { useUploadThing, uploadFiles } =
+//   generateReactHelpers<OurFileRouter>();
+
 import { OurFileRouter } from "@/app/api/uploadthing/core";
 import { generateReactHelpers } from "@uploadthing/react";
 
